e0cd8a617f2f565e1523ce61a81f213de96629ff
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-01-21 12:44:58 -0600

    Time: 20 ms (15.04%), Space: 40.4 MB (33.50%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -3,7 +3,7 @@ class Solution {
         String[] str = s.split("\\s+");
         StringBuffer sb = new StringBuffer();
         for(int i=str.length-1;i>=0;i--){
-            if(str[i].length() > 0)
+            
                 sb.append(str[i]).append(" ");
         }
         return sb.toString().trim();
