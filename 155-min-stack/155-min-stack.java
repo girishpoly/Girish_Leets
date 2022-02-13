2b0f09d152326a9926a0ae4d6c548fe21278aeb2
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-02-12 22:01:32 -0600

    Time: 10 ms (35.42%), Space: 48.1 MB (21.42%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,44 +1,31 @@
 class MinStack {
     
-    private Stack<int[]> stack = new Stack<>();
+    private Stack<Integer> val_stack = new Stack<>();
+    private Stack<Integer> min_stack = new Stack<>();
     
-    public MinStack() { }
-    
-    
-    public void push(int x) {
+    public MinStack() {
         
-        /* If the stack is empty, then the min value
-         * must just be the first value we add. */
-        if (stack.isEmpty()) {
-            stack.push(new int[]{x, x});
-            return;
-        }
-        
-        int currentMin = stack.peek()[1];
-        stack.push(new int[]{x, Math.min(x, currentMin)});
     }
     
+    public void push(int val) {
+        val_stack.push(val);
+        if (min_stack.empty()) {
+            min_stack.push(val);
+        } else {
+            min_stack.push(Math.min(min_stack.peek(), val));
+        }
+    }
     
     public void pop() {
-        stack.pop();
+        val_stack.pop();
+        min_stack.pop();
     }
-    
     
     public int top() {
-        return stack.peek()[0];
+        return val_stack.peek();
     }
-    
     
     public int getMin() {
-        return stack.peek()[1];
+        return min_stack.peek();
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
