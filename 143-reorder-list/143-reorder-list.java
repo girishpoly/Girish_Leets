65be75adf23948678f499667657db900ca331312
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-02-14 20:06:11 -0600

    Time: 5 ms (16.28%), Space: 55 MB (5.47%) - LeetHub

--- 143-reorder-list/143-reorder-list.java
+++ 143-reorder-list/143-reorder-list.java
@@ -7,6 +7,12 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
+ 
+  Intuition :
+1) Push the node in stack so that we can fetch in reverse order.
+2) Calculate half size of list.
+3) Traverse till the size, take one node from list & one from stack
+4) Assign null in stack's last node otherwise it will have the link to other nodes.
  */
 class Solution {
     public void reorderList(ListNode head) {
