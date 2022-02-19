f668502e58d1a3aa4fe9ad8bbef81f2605ac8c4a
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-02-19 13:31:28 -0600

    Time: 5 ms (86.77%), Space: 76 MB (22.45%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,17 +1,26 @@
+/**
+*The idea behind this problem is transforming the values to index (given in the problem range [1, n] inclusive.)
+then mark the place by negating it
+* Now if there is a duplicate it will also go and try to negate but find it to be already negated that means
+we got our duplicate number.
+**/
+
 class Solution {
+   
     public int findDuplicate(int[] nums) {
     
-       Arrays.sort(nums);
+        int val = 0;
         
-       for(int i = 0 ; i < nums.length ; i++){
-           
-           if(i != nums.length-1 && nums[i] == nums[i + 1]){
-               return nums[i];
-           }
-           
-       }
-        
-        return 0;
-        
+        for(int i = 0; i < nums.length; i++) {
+            val = Math.abs(nums[i]);
+            val--;
+            if(nums[val] < 0) {
+                return  Math.abs(nums[i]);
+            } else {
+                nums[val] = -nums[val];
+            }
+            
+        }
+        return -1;
     }
 }
