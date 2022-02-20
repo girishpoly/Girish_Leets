5f9056006383f4125cfe0ac833e4944479e9ac3e
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-02-20 15:29:49 -0600

    Time: 27 ms (74.90%), Space: 59.3 MB (28.57%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,35 +1,45 @@
+// Space Complexity: O(1)
+// Time Complexity: O(nlog(n)) + O(n^2)
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        //applying two pointer approach
-        Set<List<Integer>> res = new HashSet<>();
-        int n=nums.length;
-        if(n<3)
-            return new ArrayList<>(res);
-        
+        List<List<Integer>> result = new ArrayList<>();
+        int N = nums.length;
         Arrays.sort(nums);
-        for(int i=0;i<n-2;i++)
-        {
-            //Two pointer
-            int j=i+1; //first element after current element
-            int k=n-1; // last elemnet
+        
+        // If we have less than 3 element there is no valid subset
+        // If we have min value greater than 0, it's not possible to produce subset where count is zero
+        // If we have max value less than 0, it's not possible to produce subset where count is zero
+        if(N < 3 || nums[0] > 0 || nums[N-1] < 0) 
+            return result;
+        
+        for(int i = 0; i < N - 2; i++) {
+            // current i is same as previous, it will create duplicate result
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
             
-            while(j<k)
-            {
-                int sum=nums[i]+nums[j]+nums[k]; //calculating sum
-                //Three cases :
-                if(sum==0) //we have found a soln
-                {
-                    res.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                    j++; //for next element
-                    k--;
+            int left = i + 1, right = N - 1;
+            while(left < right) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if(sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // if we have duplicate elements, we need get rid of it so we are moving left and right pointer to a position
+                    // where there's no duplicate elements.
+                    while(left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while(left < right && nums[right] == nums[right - 1])
+                        right--;
+                    left++;
+                    right--;
                 }
-                
-                else if(sum<0) 
-                    j++;
-                else if(sum>0)
-                    k--;
+                else if(sum > 0)
+                    right--;
+                else
+                    left++;
             }
         }
-        return new ArrayList<>(res);
+        
+        return result;
     }
 }
