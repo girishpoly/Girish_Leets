612025ebecc3bd7519e9c2d1af2cff181a1cbd04
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-02-19 21:22:59 -0600

    Time: 1 ms (58.28%), Space: 43.1 MB (6.60%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -10,30 +10,30 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int size=0;
-        ListNode temp=head;
-        while(temp!=null){
-            size++;
-        temp=temp.next;
+         // Create two nodes pointing to head;
+        var first = head;
+        var second = head;
+        
+        // make second node n nodes ahead of first node;
+        for (int i =0; i <n; i++)
+            second = second.next;
+        
+        // If second node is null, that mean n == list.size-1;
+        // that means we have to remove the head of the list;
+        if (second == null)
+            return first.next;
+        
+        // if second node is not null, lets move both first and second nodes
+        // until second.next == null;
+        // remember after this, first node will n+1 nodes from the end;
+        while(second.next != null) {
+            first = first.next;
+            second = second.next;
         }
-
-        if(n>size){
-            return head;
-        }
-        else{
-            if(n==size){
-                head=head.next;
-            return head;
-        }
-        temp=head;
-        for(int i=1;i<size-n;i++){
-            temp=temp.next;
-        }
-
-        if(temp.next!=null){
-            temp.next=temp.next.next;
-        }
-        }
+        
+        // we can easily skip nth node and make first.next point to n.next;
+        first.next = first.next.next;
+        
         return head;
     }
 }
