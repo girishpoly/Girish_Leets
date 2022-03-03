38b5b6ef9078fa779a13d7314797db0632828f69
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-02 20:42:47 -0600

    Time: 14 ms (19.06%), Space: 43.3 MB (47.46%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,31 +1,30 @@
 class Solution {
-   public int evalRPN(String[] a) {
-  Stack<Integer> stack = new Stack<Integer>();
-  
-  for (int i = 0; i < a.length; i++) {
-    switch (a[i]) {
-      case "+":
-        stack.push(stack.pop() + stack.pop());
-        break;
-          
-      case "-":
-        stack.push(-stack.pop() + stack.pop());
-        break;
-          
-      case "*":
-        stack.push(stack.pop() * stack.pop());
-        break;
+   public int evalRPN(String[] tokens) {
+    
+       /*
+       So we can use a stack here to store numbers until they're used, 
+       and then each operand will use the top two values of the stack.
 
-      case "/":
-        int n1 = stack.pop(), n2 = stack.pop();
-        stack.push(n2 / n1);
-        break;
-          
-      default:
-        stack.push(Integer.parseInt(a[i]));
-    }
-  }
-  
-  return stack.pop();
+       Since the order of the numbers is still important for subtraction and division, 
+       we'll have to make sure that the two numbers are processed in their original order, 
+       which is the opposite order of the stack.
+
+       After each successful operation, the result should be pushed back onto the stack until it's used. 
+       After iteration is complete, the remaining value in the stack will be our answer, so we should return stack[0].
+       */
+       Set<String> ops = new HashSet<>(Arrays.asList("+", "-", "*", "/"));
+
+       Stack<Integer> stack = new Stack<>();
+        for (String t : tokens) {
+            if (!ops.contains(t)) stack.push(Integer.parseInt(t));
+            else {
+                int b = stack.pop(), a = stack.pop();
+                if (t.equals("+")) stack.push(a + b);
+                else if (t.equals("-")) stack.push(a - b);
+                else if (t.equals("*")) stack.push(a * b);
+                else stack.push(a / b);
+            }
+        }
+        return stack.pop();
 }
 }
