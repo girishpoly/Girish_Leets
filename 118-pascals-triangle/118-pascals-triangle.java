99c12ca557e621753e287a96ecfd762b0bdeedd1
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-04 20:24:42 -0600

    Time: 1 ms (66.85%), Space: 42.2 MB (12.88%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,22 +1,29 @@
+/*
+Start by creating the first row of just 1
+On each iteration, always add the first 1 and ending 1 separately from the middle numbers
+The middle numbers are just index j-1 and j from the previous row
+*/
 class Solution {
-   public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> sol = new ArrayList<>();
-        if (numRows == 0) return sol;
-        
-        List<Integer> row = new ArrayList<>();
-        row.add(1);
-        sol.add(row);
-        
-        for (int i = 1; i < numRows; i++) {
-            List<Integer> r = new ArrayList<>();
-            r.add(1);
-            List<Integer> p = sol.get(i-1);
-            for (int j = 0; j < p.size()-1; j++) {
-                r.add(p.get(j) + p.get(j+1));
-            }
-            r.add(1);
-            sol.add(r);
-        }        
-        return sol;
+  public List<List<Integer>> generate(int numRows) {
+    List<List<Integer>> result = new ArrayList<>();
+    if (numRows == 0) return result;
+
+    result.add(new ArrayList<>(Arrays.asList(1))); // seed first row
+
+    for (int i = 1; i < numRows; i++) { // starting from second row because we seeded the first
+      List<Integer> row = new ArrayList<>();
+      row.add(1); // add first 1 in each row
+	  
+      for (int j = 1; j < i; j++) {
+        int left = result.get(i-1).get(j-1);
+        int right = result.get(i-1).get(j);
+        row.add(left + right); 
+      }
+	  
+      row.add(1); // add last 1 to each row
+      result.add(row);
     }
+
+    return result;
+  }
 }
