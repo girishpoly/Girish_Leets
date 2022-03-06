6dc7019b364523bba6c41985a6f489a2ac85fb75
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-06 13:08:41 -0600

    Time: 54 ms (58.40%), Space: 54.7 MB (29.01%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -7,6 +7,7 @@ class Solution {
         }
 
         String longest = s.substring(0, 1);
+        
         for (int i = 0; i < s.length(); i++) {
             String temp = palindromeHelper(s, i, i);  // odd length
             if (temp.length() > longest.length()) {
@@ -18,16 +19,19 @@ class Solution {
                 longest = temp;
             }
         }
+        
         return longest;
     }
 
     // given a string and the CENTER(S) of the palindrome (i and i+1), find the longest possible palindrome
     // from center, go left and right till characters are same and return the longest palindromic substring
     private static String palindromeHelper(String string, int begin, int end) {
-        while (begin >= 0 && end < string.length() && string.charAt(begin) == string.charAt(end)) {
+        
+        while (begin >= 0 && end < string.length() && string.charAt(begin) == string.charAt(end))         {
             begin--;
             end++;
         }
+        
         return string.substring(begin + 1, end);
     }
 }
