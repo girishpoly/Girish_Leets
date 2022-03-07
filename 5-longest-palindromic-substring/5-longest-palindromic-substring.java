523953898829cee222e3ad1b47dc745e513b86c9
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-06 19:37:05 -0600

    Time: 27 ms (86.84%), Space: 42.4 MB (72.42%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,4 +1,7 @@
 /*
+
+Brute force is to find all the sub strings which will take O(N^2) and to check if it is palindrome O(N). Final O(N^3)
+
 Time complexity : O(n^2) Since expanding a palindrome around its center could take O(n), the overall complexity is O(n^2)
 
 Space complexity : O(1)
@@ -11,7 +14,7 @@ class Solution {
             return s;
         }
 
-        String longest = s.substring(0, 1);
+        String longest = "";
         
         for (int i = 0; i < s.length(); i++) {
             String temp = palindromeHelper(s, i, i);  // odd length
@@ -38,5 +41,8 @@ class Solution {
         }
         
         return string.substring(begin + 1, end);
+        //begin + 1 because after 0 the begin will be -1 and the substring method begin index is inclusive.
+        // we need not do to end + 1 because end will point  to s.length when it reaches end of string and the substring method end index is exclusive.
+
     }
 }
