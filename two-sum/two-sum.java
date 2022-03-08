8a0749fbec44f7f91f96141d2c9e729f89cb5324
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-08 16:37:40 -0600

    Time: 3 ms, Memory: 46.2 MB - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,29 +1,37 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
-        if(nums.length == 0){
-            return new int[0];
-        }
+        // 'res' will store the indices of the two numbers.
+    int[] res = new int[2];
+    
+    // 'map' will map each value in 'nums' to its respective index.
+    HashMap<Integer, Integer> map = new HashMap<>();
+    
+    // Perform a linear pass of 'nums' to find the solution.
+    for (int i=0; i < nums.length; i++) {
         
-        HashMap<Integer, Integer> result = new HashMap<>();
-        int[] arr = new int[2];
-
-        for(int i = 0 ; i < nums.length ; i++){
-            result.put(nums[i], i);
-        }
-        
-        for(int j = 0 ; j < nums.length ; j++){
+        // Check if we have already seen the complement of nums[i]
+        if (map.containsKey(target - nums[i])) {
+            // We have a found a pair of numbers in 'nums' which add up to 'target'.
             
-            int complement = target - nums[j];
-
-            if(result.containsKey(complement) && result.get(complement) != j){
-                arr[0] = j;
-                arr[1] = result.getOrDefault(target - nums[j], 0);
-                return arr;
-            }
+            // Our solution calls of the indices of these numbers.
+            // We have the index of nums[i] from i.
+            res[0] = i;
+            
+            // We can look up the index of the complement in 'map'.
+            res[1] = map.get(target - nums[i]);
+            
+            // We have a solution, so no need to look at the rest of 'nums'.
+            break;
+        } else {
+            // We have not seen the complement of this number yet, so let's store it in 'map'
+            // and continue looking at the rest of 'nums'.
+            map.put(nums[i], i);
         }
-        
-        return new int[0];
+    }
+    
+    // Return the two indices of the numbers which add up to 'target'.
+    return res;
         
     }
 }
