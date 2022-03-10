2873df2b82f07855e2eb7c5c87e1be564ad60a9a
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-10 12:46:42 -0600

    Time: 0 ms (100.00%), Space: 43.3 MB (32.23%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -12,8 +12,36 @@
  *         this.right = right;
  *     }
  * }
+ 
+ public int maxDepth(TreeNode root) {
+    if(root == null) {
+        return 0;
+    }
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    int count = 0;
+    while(!queue.isEmpty()) {
+        int size = queue.size();
+        while(size-- > 0) {
+            TreeNode node = queue.poll();
+            if(node.left != null) {
+                queue.offer(node.left);
+            }
+            if(node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        count++;
+    }
+    return count;
+}
  */
 class Solution {
+    /*
+    Time complexity : we visit each node exactly once, thus the time complexity is O(N), where NN is the number of nodes.
+
+Space complexity : in the worst case, the tree is completely unbalanced, e.g. each node has only left child node, the recursion call would occur N times (the height of the tree), therefore the storage to keep the call stack would be O(N). But in the best case (the tree is completely balanced), the height of the tree would be log(N). Therefore, the space complexity in this case would be O(log(N)).
+    */
     public int maxDepth(TreeNode root) {
            // Base Condition
         if(root == null) return 0;
