1eb6ea248799f8d9a8b77d8d179421cb7827989a
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-10 20:34:06 -0600

    Time: 2 ms (72.37%), Space: 44.3 MB (53.40%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,4 +1,21 @@
-	class Solution {
+/*
+// basic idea is to keep track of current maximum , current minimum and global maximum
+//current minimum may result in maximum value too if both the number multiplied are negative
+
+        int currSum = nums[0];
+        int maxSum = nums[0];
+        
+        for(int i = 1 ; i < nums.length ;i++){
+            
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(maxSum, currSum);
+            
+        }
+        
+        return maxSum;
+*/	
+
+class Solution {
 				public int maxProduct(int[] nums) {
 				int max = nums[0] , curMax = nums[0] ,curMin = nums[0] , temp,i;
 				for(i=1;i<nums.length;i++)
