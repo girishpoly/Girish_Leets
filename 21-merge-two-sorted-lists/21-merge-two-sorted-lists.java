7181e33c6937702a475065874dda0c00e07d6076
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-10 20:07:28 -0600

    Time: 1 ms (72.74%), Space: 43.2 MB (27.13%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -11,16 +11,16 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
          //Define a new Node
-        ListNode answer = new ListNode();
-        ListNode newNode = answer;
+        ListNode result = new ListNode();
+        ListNode curr = result;
         
         //If list1 has no element then add list2 to new node
         if (list1 == null)
-           return newNode.next = list2;
+           return curr.next = list2;
         
         //If list2 has no element then add list1 to new node
         if (list2 == null)
-            return newNode.next = list1;
+            return curr.next = list1;
         
         
         //Use Merge sort logic to merge nodes value
@@ -28,25 +28,25 @@ class Solution {
         {
             if (list1.val <= list2.val)
             {
-                newNode.next = list1;
+                curr.next = list1;
                 list1 = list1.next;
             }
             else 
             {
-                newNode.next = list2;
+                curr.next = list2;
                 list2 = list2.next;
             }
-            newNode = newNode.next;
+            curr = curr.next;
         }
     
         //If any node left in list1
         if (list1 != null)
-            newNode.next = list1;
+            curr.next = list1;
      
         //If any node left in list2
         if (list2 != null)
-            newNode.next = list2;
+            curr.next = list2;
         
-        return answer.next;
+        return result.next;
     }
 }
