f5ba54770500eca39a99670c60c9dbf883adf8ed
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-10 20:56:44 -0600

    Time: 8 ms (44.87%), Space: 59.5 MB (21.02%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -12,6 +12,14 @@
  *         this.right = right;
  *     }
  * }
+ 
+ When asked to find information about a particular row of a binary tree, the normal thought is to use a breadth-first search (BFS) approach. A BFS approach usually involves the use of a queue data structure (q) so that we deal with the nodes of the tree in the proper order.
+
+The trick is to deal with a single row at a time by making note of the length of the queue (qlen) when we start the row. Once we've processed that many nodes, we know we've just finished the current row and any remaining entries in q are from the next row. This can be accomplished easily with a nested loop.
+
+In this case, processing a node simply means accumulating the running total (ans) for the row and then moving any children of the node onto the end of the queue.
+
+When we start a new row, we can reset ans back to 0, and then just keep processing rows until q is empty. The last value of ans should be our final answer, so we should return ans.
  */
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
