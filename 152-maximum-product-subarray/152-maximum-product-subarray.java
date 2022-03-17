3664d47e79dc7a47d197b9888e5fb20e4677fbe2
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-16 22:38:27 -0500

    Time: 2 ms (71.87%), Space: 45.1 MB (24.10%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -13,20 +13,36 @@
         }
         
         return maxSum;
+        
+Case1 :- All the elements are positive : Then your answer will be product of all the elements in the array.
+
+Case2 :- Array have positive and negative elements both :
+If the number of negative elements is even then again your answer will be complete array because on multiplying all the negative numbers it will become positive.
+If the number of negative elements is odd then you have to remove just one negative element and for that u need to check your subarrays to get the max product.
+
+Case3 :- Array also contains 0 : Then there will be not much difference...its just that your array will be divided into subarray around that 0. What u have to so is just as soon as your product becomes 0 make it 1 for the next iteration, now u will be searching new subarray and previous max will already be updated.
+
+For each index i keep updating the max and min. We are also keeping min because on multiplying with any negative number your min will become max and max will become min. So for every index i we will take max of (i-th element, prevMax * i-th element, prevMin * i-th element).
 */	
 
 class Solution {
-				public int maxProduct(int[] nums) {
-				int max = nums[0] , curMax = nums[0] ,curMin = nums[0] , temp,i;
-				for(i=1;i<nums.length;i++)
-				{
-					temp =curMax;
-					curMax = Math.max(nums[i] , Math.max(nums[i]*curMax , nums[i]*curMin));
-					curMin = Math.min(nums[i] , Math.min(nums[i]*temp , nums[i]*curMin));
+    public int maxProduct(int[] nums) {
+        
+        int max = nums[0], min = nums[0], ans = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            
+            int temp = max;  // store the max because before updating min your max will already be updated
+            
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+            
+            if (max > ans) {
+                ans = max;
+            }
+        }
+        
+        return ans;
 
-					max = Math.max(max , curMax);
-				}
-
-				return max;
-				}
-			}
+    }
+}
