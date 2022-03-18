82253697b177fa4c67df9c11983e7607bd51bf61
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-17 22:28:50 -0500

    Time: 12 ms (65.89%), Space: 56.1 MB (38.48%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,27 +1,25 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
-        HashMap<String, List> map = new HashMap<>();
-        
-        for(String str : strs){
-            
-            String sortedStr = sortString(str);
-            
-            if(!map.containsKey(sortedStr)){
-                map.put(sortedStr, new ArrayList());
+        List<List<String>> result =new ArrayList<>();
+        HashMap<String,List<String>> map=new HashMap<>();
+		
+        for(int i=0;i<strs.length;i++){
+            String s=sortStr(strs[i]);// sort the string |  every anagram will return same string
+            if(!map.containsKey(s)){
+                map.put(s,new ArrayList<String>()); // psuh that sorted string as key 
             }
-            
-            map.get(sortedStr).add(str);
-            
+            map.get(s).add(strs[i]);// add in the list  
         }
-        
-        return new ArrayList(map.values());
-        
+        for(List<String> list : map.values()){
+            result.add(list);
+        }
+        return result;
+    }
+    public String sortStr(String s){
+        char c[]=s.toCharArray();
+        Arrays.sort(c);
+        String s1=new String(c);
+        return s1;
     }
     
-    public String sortString(String s){
-        char[] c = s.toCharArray();
-        Arrays.sort(c);
-        return new String(c);   
-    }
 }
