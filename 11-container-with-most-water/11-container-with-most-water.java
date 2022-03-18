daf1745be9513102e8d7ef75743c1fb06f63b432
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-17 22:00:33 -0500

    Time: 4 ms (72.63%), Space: 81.6 MB (17.12%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxArea(int[] height) {
+        //since the points are sorted we can use two pointers start and end
         
         int start = 0; 
         int end = height.length - 1;
@@ -7,6 +8,8 @@ class Solution {
         
         while(start < end){
             
+            //if height[start] is smaller then consider it to calculate the 
+            //area. length * width.
             if(height[start] < height[end]){
                 maxArea = Math.max(maxArea, height[start] * (end - start));
                 start++;
