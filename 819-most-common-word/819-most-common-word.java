09abadd7d29c14f9e103121f200fa4c0309568fd
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-18 22:00:50 -0500

    Time: 13 ms (81.08%), Space: 43 MB (65.08%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        Set<String> set = new HashSet<>();
+        for(String s : banned) {
+            set.add(s); 
+        }
+        
+        Map<String, Integer> map = new HashMap<>();
+        String[] strs = paragraph.toLowerCase().split("\\W+");
+        int max = 0;
+        String res = "";
+
+        for(String str : strs) {
+            if(!set.contains(str)) {
+                map.put(str, map.getOrDefault(str, 0) + 1);
+            }
+        }
+        
+        for(String key : map.keySet()) {
+            if(map.get(key) > max) {
+                max = map.get(key);
+                res = key;
+            }
+        }
+        
+        return res;
+    }
+}
