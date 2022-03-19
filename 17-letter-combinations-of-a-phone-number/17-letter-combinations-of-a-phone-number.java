9c0f5f1933c9301cd5ca22eff1db699fa60235ee
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-19 17:34:28 -0500

    Time: 1 ms (92.04%), Space: 42 MB (67.84%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,43 +1,49 @@
+/*
+This is the explanation of the solution:
+
+Create a dictionary to hold the mapping of phone number to digit.
+Ex: 2 means "abc", 3 means "def" and so on.
+If digits length is zero -> return empty array list
+We will go through the digits one by one.
+Ex: for "23", first 2, then 3.
+We will use a helper function called combine.
+combine will take character array of all the letters corresponding to current digit and the result up till now.
+We will run 2 loops - outer loop goes through each character in the char array like 'a', 'b', 'c'. and inner loop will go through already present strings in result like "ad", "ae", "af" etc.
+Will simply concatenate and store in new array list.
+Return result
+*/
+
 class Solution {
     public List<String> letterCombinations(String digits) {
-        // build up the HashMap Charater to String
-        Map<Character, String> phone = new HashMap<Character, String>() {{
-            put('2', "abc");
-            put('3', "def");
-            put('4', "ghi");
-            put('5', "jkl");
-            put('6', "mno");
-            put('7', "pqrs");
-            put('8', "tuv");
-            put('9', "wxyz");
-          }};
         
-        List<String> ls = new ArrayList<>();
-        // if "digits" is empty return empty list
-        if(digits.length() == 0) {
-            return ls;
+        if(digits.length()==0)    return new ArrayList<String>();
+        
+        String[] dict = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
+        ArrayList<String> result = new ArrayList<>();
+        
+        for(int i=0; i<digits.length(); i++) {
+            result = combine(dict[digits.charAt(i)-'0'].toCharArray(), result);
         }
         
-        StringBuilder sb = new StringBuilder();
-        backtrack(sb, digits, ls, phone, 0);
-        return ls;
+        return result;
     }
     
-    public void backtrack(StringBuilder sb, String digits, List<String> ls,                   Map<Character, String> phone, int start) {
-        if(sb.length() == digits.length()) {
-            ls.add(sb.toString());
-            return;
+    public ArrayList<String> combine(char[] toBeAdded, ArrayList<String> result) {
+        
+        if (result.isEmpty()) {
+            for(char tba: toBeAdded)
+                result.add(String.valueOf(tba));
+            return result;
         }
-        // we have to use 'start' to ensure we will go to next character of digit
-        for(int i = start; i < digits.length(); i++) {
-            // get the character's corresponding String from the map
-            String s = phone.get(digits.charAt(i));
-            // loop through all the character in specific String in Map
-            for(int j = 0; j < s.length(); j++) {
-                sb.append(s.charAt(j));
-                backtrack(sb, digits, ls, phone, i + 1);
-                sb.deleteCharAt(sb.length() - 1);
-            }
+        
+        ArrayList<String> result2 = new ArrayList<String>();
+        
+        for(String res: result) {
+            for(char tba: toBeAdded)
+                result2.add(res+String.valueOf(tba));
         }
+        
+        return result2;
     }
 }
