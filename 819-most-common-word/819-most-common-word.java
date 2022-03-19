b5205af4c454e16b95e83342aa2952f2f05f0c5c
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-18 22:08:42 -0500

    Time: 220 ms (21.54%), Space: 45.3 MB (13.25%) - LeetHub

--- 819-most-common-word/819-most-common-word.java
+++ 819-most-common-word/819-most-common-word.java
@@ -6,9 +6,16 @@ class Solution {
         }
         
         Map<String, Integer> map = new HashMap<>();
+        
+        // 1). replace the punctuations with spaces,
+        // and put all letters in lower case
+                // 2). split the string into words
+
         String[] strs =  paragraph.replaceAll("[^a-zA-Z0-9 ]", "              ").toLowerCase().split("\\s+");
         int max = 0;
         String res = "";
+
+                // 3). count the appearance of each word, excluding the banned words
 
         for(String str : strs) {
             if(!set.contains(str)) {
@@ -16,6 +23,7 @@ class Solution {
             }
         }
         
+                // 4). return the word with the highest frequency
         for(String key : map.keySet()) {
             if(map.get(key) > max) {
                 max = map.get(key);
