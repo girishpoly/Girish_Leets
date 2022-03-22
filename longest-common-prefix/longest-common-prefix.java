11cd2ddc2577c3051130a891ebd01d6629fd0cf9
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-22 12:00:33 -0500

    Time: 0 ms, Memory: 42.7 MB - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for(int index=1;index<strs.length;index++){
+            while(strs[index].indexOf(prefix) != 0){
+                prefix=prefix.substring(0,prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+}
+
+/*
+Working:
+1)Take the first(index=0) string in the array as prefix.
+2)Iterate from second(index=1) string till the end.
+3)Use the indexOf() function to check if the prefix is there in the strs[i] or not.
+If the prefix is there the function returns 0 else -1.
+4)Use the substring function to chop the last letter from prefix each time the function return -1.
+*/
+
