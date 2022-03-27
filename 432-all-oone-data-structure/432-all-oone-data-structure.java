7fec2e41d046b5d3a48785c630579ced4fdfd226
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-27 18:04:13 -0500

    Time: 292 ms (6.00%), Space: 59.2 MB (84.53%) - LeetHub

--- 432-all-oone-data-structure/432-all-oone-data-structure.java
+++ 432-all-oone-data-structure/432-all-oone-data-structure.java
@@ -1,45 +1,85 @@
+/*
+can be done using hashmap and priority queue by maintaing max heap and min heap. But it won't be O(1).
+https://leetcode.com/problems/all-oone-data-structure/discuss/1096482/JAVA-Hashmap-%2B-Heap-(min-and-max-pq)
+
+
+*/
 class AllOne {
-    
-    private final Map<String, Integer> keyToCount = new HashMap<>();
-    private final TreeMap<Integer, Set<String>> countToKey = new TreeMap<>();
-
-    public AllOne() {
-    }
-    
-    public void inc(String key) {
-        var count = keyToCount.merge(key, 1, Integer::sum);
-        moveWord(count, count - 1, key);
-    }
-    
-    public void dec(String key) {
-        var count = keyToCount.merge(key, -1, Integer::sum);
-        if (count == 0)
-            keyToCount.remove(key);
-        
-        moveWord(count, count + 1, key);
-    }
-    
-    public String getMaxKey() {
-        return countToKey.isEmpty() ? "" : countToKey.lastEntry().getValue().iterator().next();
-    }
-    
-    public String getMinKey() {
-        return countToKey.isEmpty() ? "" : countToKey.firstEntry().getValue().iterator().next();
-    }
-    
-    private void moveWord(int nextCount, int prevCount, String key) {
-        if (nextCount != 0)
-            countToKey.computeIfAbsent(nextCount, v -> new HashSet<>()).add(key);
-
-        if (prevCount != 0) {
-            var keys = countToKey.get(prevCount);
-            if (keys.size() == 1)
-                countToKey.remove(prevCount);
-            else
-                keys.remove(key);
+    class Entry{
+        int count;
+        String key;
+        public Entry(String key){
+            this.key = key;
+            count = 1;
         }
     }
+    HashMap<String, Entry> entries; 
+    PriorityQueue<Entry> maxHeap;
+    PriorityQueue<Entry> minHeap;
+
+    /** Initialize your data structure here. */
+    public AllOne() {
+        entries = new HashMap();
+        maxHeap = new PriorityQueue(new Comparator<Entry>() {
+            public int compare(Entry a, Entry b) {
+                return Integer.compare(b.count, a.count);
+            }
+        });
+        minHeap = new PriorityQueue(new Comparator<Entry>() {
+            public int compare(Entry a, Entry b) {
+                return Integer.compare(a.count, b.count);
+            }
+        });
+    }
+    
+    /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
+    public void inc(String key) {
+        Entry e;
+        if(entries.containsKey(key)){
+            e = entries.get(key);
+            e.count++;
+            maxHeap.remove(e);
+            minHeap.remove(e);
+        }    
+        else{
+            e = new Entry(key);
+            entries.put(key, e);
+        }
+        minHeap.add(e);
+        maxHeap.add(e);
+    }
+    
+    /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
+    public void dec(String key) {
+        Entry e;
+        if(entries.containsKey(key)){
+            e = entries.get(key);
+            e.count--;
+            maxHeap.remove(e);
+            minHeap.remove(e);
+            if(e.count==0){
+                entries.remove(e);    
+            }
+            else{
+                minHeap.add(e);
+                maxHeap.add(e);   
+            }
+        }
+    }
+    
+    /** Returns one of the keys with maximal value. */
+    public String getMaxKey() {
+        if(maxHeap.size()==0) return "";
+        else return maxHeap.peek().key;
+    }
+    
+    /** Returns one of the keys with Minimal value. */
+    public String getMinKey() {
+        if(minHeap.size()==0) return "";
+        else return minHeap.peek().key;    
+    }
 }
+
 /**
  * Your AllOne object will be instantiated and called as such:
  * AllOne obj = new AllOne();
