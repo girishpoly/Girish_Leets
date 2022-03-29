596f4ea170af0fd723450b89b4d78a1ab943828b
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-29 17:51:01 -0500

    Time: 3 ms (97.72%), Space: 42 MB (50.44%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
@@ -1,5 +1,14 @@
 /*
 https://leetcode.com/problems/partition-to-k-equal-sum-subsets/discuss/1772704/Java-Solution-(with-comments)-oror-100-faster-oror-1ms
+
+
+Let NN be the number of elements in the array and kk be the number of subsets.
+
+Time complexity: O(k 2^n)
+
+We are traversing the entire array for each subset (once we are done with one subset, for the next subset we are starting again with index 0). So for each subset, we are choosing the suitable elements from the array (basically iterate over nums and for each element either use it or skip it,
+
+Space complexity: O(N)
 */
 class Solution {
     public boolean canPartitionKSubsets(int[] nums, int k) {
