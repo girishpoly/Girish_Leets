f2e086137a3c678c25f330f70acd17cd472d61d4
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-28 22:57:50 -0500

    Time: 1 ms (92.51%), Space: 42.3 MB (92.07%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -5,23 +5,25 @@ https://leetcode.com/problems/permutations/discuss/1861293/Simple-Java-Solution-
 */
 
 class Solution {
-   public List<List<Integer>> permute(int[] nums) {
-   List<List<Integer>> list = new ArrayList<>();
-   // Arrays.sort(nums); // not necessary
-   backtrack(list, new ArrayList<>(), nums);
-   return list;
-}
-
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
-   if(tempList.size() == nums.length){
-      list.add(new ArrayList<>(tempList));
-   } else{
-      for(int i = 0; i < nums.length; i++){ 
-         if(tempList.contains(nums[i])) continue; // element already exists, skip
-         tempList.add(nums[i]);
-         backtrack(list, tempList, nums);
-         tempList.remove(tempList.size() - 1);
-      }
-   }
-} 
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        backtrack(list, new ArrayList<>(), nums);
+        return list; 
+        
+    }
+    
+    public void backtrack(List<List<Integer>> list, List<Integer> temp, int [] nums){
+        if (temp.size()==nums.length){
+            list.add(new ArrayList(temp));//if the the temp list equals nums length then add to resultant list
+            }else{
+            for(int i=0;i<nums.length;i++){
+                if(temp.contains(nums[i]))
+                    continue;
+                temp.add(nums[i]);//Add the current traversing element from nums
+                backtrack(list, temp, nums);//Backtrack to its previous node by calling backtrack function recursively
+                temp.remove(temp.size()-1);//remove the last element of the temp to find another way from the prev node which we got ofter backtracking 
+            }
+        }
+    }
+    
 }
