73edb864b654ac240a5694dddbba8a95194f5ebd
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-29 17:58:18 -0500

    Time: 1 ms (100.00%), Space: 39.7 MB (92.66%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
@@ -12,34 +12,56 @@ Space complexity: O(N)
 */
 class Solution {
     public boolean canPartitionKSubsets(int[] nums, int k) {
-        // Not possible to divide into equal subsets, if sum of all 
-        // nums[] is not a multiple of k.
-        int sum = 0;
-        for (int i = nums.length - 1; i >= 0; i--)  sum += nums[i];
-        if ((sum % k) != 0)  return false;
+        int sum=0;
+        for(int i:nums){
+            sum+=i;
+        }
         
-        // Determine the target number that each subset must total.
-        // Then start recursion to find if possible to have k equal 
-        // subsets.
-        int target = sum / k;
-        return dfs(k, 0, 0, nums, target);
+        //sum%k must equal to 0 if not just return false
+        //if we have to to divide the array greater than array size retun false(we can't)
+        if(sum%k!=0 || nums.length<k) return false;
+        
+        //sort so we can take last element and start filling our bucket
+        Arrays.sort(nums);
+        
+        //our target is sum/k and we have to find this in nums, k times then it is valid
+        return canPartitionKSubsets(nums,sum/k,nums.length-1,new int[k]);
+    
     }
-    
-    
-    private boolean dfs(int kRemainingToFill, int curSum, int numsIdx, int[] nums, int target) {
-        if (kRemainingToFill <= 0)  return true;        // If no more subsets to fill, then done.
-        for (int i = numsIdx; i < nums.length; i++) {   // Loop in nums[] values to find next unused.
-            if (nums[i] != 0 && curSum + nums[i] <= target) {
-                int temp = nums[i];
-                nums[i] = 0;                            // Mark this nums value as "used".
-                if (curSum + temp == target) {          // If subset exactly filled, start new subset.
-                    if (dfs(kRemainingToFill - 1, 0, 0, nums, target))  return true;
-                } else {                                // Else subset not filled, find more to fill it.
-                    if (dfs(kRemainingToFill, curSum + temp, i + 1, nums, target))  return true;
-                }
-                nums[i] = temp;                         // This nums[i] value didn't result in a good 
-            }                                           //   solution, so "unuse" this nums[] value and  
-        }                                               //   loop back to try another nums[] value
+    public boolean canPartitionKSubsets(int a[],int target,int i,int bucket[]){
+        
+        //we have taken all the elements
+        if(i==-1)
+            return true;
+        
+        //start filling the buckets
+        for(int j=0;j<bucket.length;j++){
+            
+            //can we take this ith element
+            if(bucket[j]+a[i]<=target){
+            
+                //if we take this element
+                bucket[j]+=a[i];
+                
+                //go to next element (in our case go to smallest ele bcz we sorted)
+                //if we can fill all buckets then return true
+                if(canPartitionKSubsets(a,target,i-1,bucket))
+                    return true;
+                
+                //means we can't fill other buckets if we take ith element so leave this element
+                bucket[j]-=a[i];
+            
+            }
+            
+            //if our bucket is empty means we have not taken any elements in the buckets
+            if(bucket[j]==0)
+                break;
+        
+        }
+        
+        //all buckets are full but i is pointing to some element (elements still left)
+        //or our bucket is empty means we haven't take any element (not valid)
         return false;
+    
     }
 }
