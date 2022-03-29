d222f8d8a29248d9b8efdd06f1842feed1d76902
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-03-28 22:34:59 -0500

    Time: 3 ms (42.00%), Space: 44.4 MB (71.29%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -5,25 +5,23 @@ https://leetcode.com/problems/permutations/discuss/1861293/Simple-Java-Solution-
 */
 
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        return permutation(new ArrayList<>(),nums);
-    }
-    public List<List<Integer>> permutation(List<Integer> p,int[] up){
-        
-        if(up.length==0){
-            List<List<Integer>> sublist=new ArrayList<>();
-            sublist.add(p);
-            return sublist;
-        }
-        int num=up[0];
-        List<List<Integer>> ans=new ArrayList<>();
-        for(int i=0;i<=p.size();i++){
-            List<Integer> new1=new ArrayList<>();
-            new1.addAll(p.subList(0,i));
-            new1.add(num);
-            new1.addAll(p.subList(i,p.size()));
-            ans.addAll(permutation(new1,Arrays.copyOfRange(up, 1, up.length)));
-        }
-        return ans;
-    }
+   public List<List<Integer>> permute(int[] nums) {
+   List<List<Integer>> list = new ArrayList<>();
+   // Arrays.sort(nums); // not necessary
+   backtrack(list, new ArrayList<>(), nums);
+   return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+   if(tempList.size() == nums.length){
+      list.add(new ArrayList<>(tempList));
+   } else{
+      for(int i = 0; i < nums.length; i++){ 
+         if(tempList.contains(nums[i])) continue; // element already exists, skip
+         tempList.add(nums[i]);
+         backtrack(list, tempList, nums);
+         tempList.remove(tempList.size() - 1);
+      }
+   }
+} 
 }
