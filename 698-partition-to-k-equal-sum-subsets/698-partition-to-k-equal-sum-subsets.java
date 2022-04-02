dcf2c24c2a013989b4dd97466ce7bea81c79dbc9
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-04-02 17:20:39 -0500

    Time: 2745 ms (5.02%), Space: 42.1 MB (41.73%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
@@ -9,6 +9,37 @@ Time complexity: O(k 2^n)
 We are traversing the entire array for each subset (once we are done with one subset, for the next subset we are starting again with index 0). So for each subset, we are choosing the suitable elements from the array (basically iterate over nums and for each element either use it or skip it,
 
 Space complexity: O(N)
+
+
+nums = [4,3,2,3,5,2,1]
+tottal = 20 
+target = 20/4 = 5
+
+we need to find k subsets in the array that add up to 5
+
+start with 4 and do a scan if we can add any other element in the nums and can reach target sum = 5
+if the sum is greater return 
+if the sum is less, keep going until we find 1
+we found 4 + 1 = 5
+we need to mark 4 and 1 as visited so we don't use it again 
+reduce k by 1 since we found 1 solution.
+after finding the solution reset the curSum and subsetSum
+
+check each time if a number is visited
+next time we skip 4 and start with 3 
+we found 3 + 2 = 5 
+mark both 3 and 2 as visited
+k = 2
+reset curSum = 0 
+
+skip 4,3,2 and start with 3 
+we can find 3 + 2 = 5 
+reduce k = 1
+reset curSum = 0
+so far we have 4,3,2,2,2 as visited
+
+we find 5 equal to subset sum , k = 0 
+k == 0 return true we found the solution
 */
 class Solution {
     public boolean canPartitionKSubsets(int[] nums, int k) {
