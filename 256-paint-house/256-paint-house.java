9805630e466ac19c070f2e222ae3ab17cc41c421
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-04-02 16:54:47 -0500

    Time: 2 ms (38.25%), Space: 43.4 MB (50.08%) - LeetHub

--- 256-paint-house/256-paint-house.java
+++ 256-paint-house/256-paint-house.java
@@ -16,13 +16,14 @@ So it would be 2^n or (m-1)^n where m#colors n#houses in general. if you draw th
 
 The base case would be we assume we paint House 0( 0 based indexing) with given 3 possible colors
 
-Time Complexity : O(n).
+Time complexity : O(nm^2)
+we iterate over each of n.m cells. for each cell we are finding the minimum of the k values in the row above. multiply by O(m). O(nm^2)
 
+Space complexity : O(1) in place
 Finding the minimum of two values and adding it to another value is an O(1) operation. We are doing these O(1) operations for 3(n - 1) cells in the grid. Expanding that out,3⋅n−3. The constants don't matter in big-oh notation, so we drop them, leaving us with O(n).
 
 A word of warning: This would not be correct if there were mm colors. For this particular problem we were told there's only 33 colors. However, a logical follow-up question would be to make the code work for any number of colors. In that case, the time complexity would actually be O(n⋅m), because mm is not a constant, whereas 3 is. .
 
-Space Complexity : O(1)
 */
 class Solution {
     public int minCost(int[][] costs) {
