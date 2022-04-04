565eb44ef8078b65f8b8f566563d14aab81c1105
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-04-04 17:02:27 -0500

    Time: 2 ms, Memory: 51.9 MB - LeetHub

--- can-place-flowers/can-place-flowers.java
+++ can-place-flowers/can-place-flowers.java
@@ -6,7 +6,10 @@ class Solution {
 		    //for including any flower previous and next should also be empty.
 			//in case of 0th index we have to check only next index.
 			//in case of last index we have to check only previous index.
-            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbed[i+1]==0)) {
+            if(flowerbed[i]==0 && 
+               (i==0 || flowerbed[i-1]==0) && 
+               (i==flowerbed.length-1 || flowerbed[i+1]==0)) {
+                
                 c++;
 				
 				//if any index is included, then next index can't be part of our solution, 
