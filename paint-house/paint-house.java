c42a6a5ed804baf6d5b92831bd18d996bd0c8ebd
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-04-04 17:19:25 -0500

    Time: 2 ms, Memory: 44.2 MB - LeetHub

--- paint-house/paint-house.java
+++ paint-house/paint-house.java
@@ -1,6 +1,13 @@
 //https://leetcode.com/problems/paint-house/discuss/1787489/JAVA-Multiple-approaches-explained-with-comments
 
+
+
 /*
+The brute force way is to generate every valid permutation, remove the invalid ones and find the best one.
+The time complexity will be exponential. O(2^n)
+
+
+
 Using dynamic programming 2D-tabulation dp[N][M] N- Number of Houses M-Number of Colors to choose from
 
 This problem can be solved using dynamic programming in either bottom up or top down approach. Represented bewlow is a top down approach using 2d tabulation. Using btute force approach there would be 2^n possible ways in which you can paint n houses with given m=3 colors as at each index(house) you have 3 options for the first house and 2 options for all other houses.
@@ -9,13 +16,14 @@ So it would be 2^n or (m-1)^n where m#colors n#houses in general. if you draw th
 
 The base case would be we assume we paint House 0( 0 based indexing) with given 3 possible colors
 
-Time Complexity : O(n).
+Time complexity : O(nm^2)
+we iterate over each of n.m cells. for each cell we are finding the minimum of the k values in the row above. multiply by O(m). O(nm^2)
 
+Space complexity : O(1) in place
 Finding the minimum of two values and adding it to another value is an O(1) operation. We are doing these O(1) operations for 3(n - 1) cells in the grid. Expanding that out,3⋅n−3. The constants don't matter in big-oh notation, so we drop them, leaving us with O(n).
 
 A word of warning: This would not be correct if there were mm colors. For this particular problem we were told there's only 33 colors. However, a logical follow-up question would be to make the code work for any number of colors. In that case, the time complexity would actually be O(n⋅m), because mm is not a constant, whereas 3 is. .
 
-Space Complexity : O(1)
 */
 class Solution {
     public int minCost(int[][] costs) {
