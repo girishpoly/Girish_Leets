4a702654dc659b13b105dac531ad11744517cae9
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-04-04 17:09:56 -0500

    Time: 0 ms, Memory: 40.6 MB - LeetHub

--- find-leaves-of-binary-tree/find-leaves-of-binary-tree.java
+++ find-leaves-of-binary-tree/find-leaves-of-binary-tree.java
@@ -12,46 +12,139 @@
  *         this.right = right;
  *     }
  * }
+ 
+ o(n), space is for recursive calls O(n)
+ 
+ Do a DFS to go all the way to the leaf nodes. IF it is a leaf node meaning , if left node and right node are null then it is a leaf node.
+ all leaf nodes to templist and add templist to result for every iteration.
+ once the leaf node is processed mark it as null.
+ 
+ DFS - go through entire depth of the tree , remove leaf nodes and backtrack
+ 
+ at every iteration mark the leaves as null so we don't process them again. 
+ 
+     1
+  2     3
+  
+4   5
+
+res : []
+root = 1 tempList: []
+node = 1
+
+     1                   
+  2     3        
+  
+4   5
+ 
+res : []
+root = 1 tempList: [4]
+node = 1
+mark 4 as null
+
+     1                   
+  2     3        
+  
+n   n
+
+res : []
+root = 1 tempList: [4,5]
+node = 1
+mark 5 as null
+
+     1                   
+  2     n        
+  
+n   n
+
+res : []
+root = 1 tempList: [4,5,3]
+node = 1
+mark 3 as null
+
+     1                   
+  2     n        
+  
+n   n
+
+res : [[4,5,3],]
+root = 1 tempList: [4,5,3]
+node = 1
+mark 3 as null
+
+
+again recursivley call DFS with node as 1
+
+     1                   
+  2n     n        
+  
+n   n
+
+res : [[4,5,3], [2]]
+root = 1 tempList: [2]
+node = 1
+
+2 is leaf node , mark it as null and return back to 1
+
+
+     1                   
+  n     n        
+  
+n   n
+
+res : [[4,5,3], [2] , [1]]
+root = 1 tempList: [1]
+node = 1
+
+1 is only leaf node , mark it as null
+
+
+     n                  
+  n     n        
+  
+n   n
+res : [[4,5,3], [2] , [1]]
+
+root becomes null and exits while loop
  */
 class Solution {
     public List<List<Integer>> findLeaves(TreeNode root) {
+        
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> leaves = null; 
+        if(root == null){
+            return result;
+        }
+        
         while(root != null){
-              if(root !=null &&  root.left == null && root.right == null){
-                leaves = new ArrayList<>(); 
-                leaves.add(root.val);
-                result.add(leaves);
-                break;
-            }
-            
-            leaves = new ArrayList<>(); 
-            dfs(root,leaves);
-            result.add(leaves);
-          
+            List<Integer> tmpList = new ArrayList();
+            //create a tmpList for each iteration and pass it to our helper method so it gets filled with the leaf nodes
+            //for every iteration
+            root = dfs(root, tmpList);
+            result.add(tmpList);
         }
         
         return result;
-    }
-    public void dfs(TreeNode node, List<Integer> leaves){
-        if(node == null) return;
         
-        if(node.left != null){
-            if(node.left.left == null && node.left.right == null){
-                leaves.add(node.left.val);
-                node.left = null;
-            }else{
-                 dfs(node.left,leaves);
-            }
+    }
+    
+    
+    private TreeNode dfs(TreeNode node, List<Integer> list){
+        
+        if(node == null){
+            return null;
         }
-        if(node.right != null){
-            if(node.right.left == null && node.right.right == null){
-                leaves.add(node.right.val);
-                node.right = null;
-            }else{
-                dfs(node.right,leaves);
-            }
+        
+        //leaf node
+        if(node.left == null && node.right == null){
+            list.add(node.val);
+            node = null;
+            return node;
         }
+        
+        //not leaf node
+        node.left = dfs(node.left, list);
+        node.right = dfs(node.right, list);
+        return node;
         
     }
 }
