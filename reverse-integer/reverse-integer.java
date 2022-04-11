6a899c53c0ba34b959e18add1643adc0c5362631
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-04-10 23:51:11 -0500

    Time: 1 ms, Memory: 42.1 MB - LeetHub

---
+++ reverse-integer/reverse-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        long num = 0;
+        while(x!=0){
+            num = num*10 + x%10;
+            x=x/10;
+        }
+        if(num>=Integer.MIN_VALUE && num<=Integer.MAX_VALUE)
+        return (int)num;
+        else
+            return 0;
+    }
+}
