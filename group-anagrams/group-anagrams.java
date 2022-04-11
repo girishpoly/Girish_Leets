170eec593f9b043d4ba4ad0c6ed6e9290e10510f
girishpoly <97372510+girishpoly@users.noreply.github.com>
2022-04-10 23:41:08 -0500

    Time: 5 ms, Memory: 45.4 MB - LeetHub

--- group-anagrams/group-anagrams.java
+++ group-anagrams/group-anagrams.java
@@ -1,32 +1,24 @@
-/*
-Time Complexity: O(NKlogK), where N is the length of strs, and K is the maximum length of a string in strs. The outer loop has complexity O(N) as we iterate through each string. Then, we sort each string in O(KlogK) time.
-
-Space Complexity: O(NK), the total information content stored in ans.
-*/
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result =new ArrayList<>();
         HashMap<String,List<String>> map=new HashMap<>();
-		
-        for(int i=0;i<strs.length;i++){
-            String s=sortStr(strs[i]);// sort the string |  every anagram will return same string
-            if(!map.containsKey(s)){
-                map.put(s,new ArrayList<String>()); // psuh that sorted string as key 
-            }
-            map.get(s).add(strs[i]);// add in the list  
-        }
         
-        for(List<String> list : map.values()){
-            result.add(list);
+        for(int i=0;i<strs.length;i++){
+            String s1=strs[i];
+            char[] arr=s1.toCharArray();
+            Arrays.sort(arr);
+            String str=new String(arr);
+            
+            if(map.containsKey(str)){
+                map.get(str).add(s1); 
+            }else{
+                map.put(str,new ArrayList<>());
+                map.get(str).add(s1);
+            }
         }
-        return result;
+        return new ArrayList<>(map.values());
     }
-    
-    public String sortStr(String s){
-        char c[]=s.toCharArray();
-        Arrays.sort(c);
-        String s1=new String(c);
-        return s1;
-    }
-    
 }
+
+/*
+Time Complexity: O(n * klog(k)) since we are sorting k characters n times in the loop.
+*/
